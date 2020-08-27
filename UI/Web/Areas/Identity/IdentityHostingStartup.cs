426647f945e58bf8ad938bdd048128b8504ec1a2
Diego Lopez <diegol2k@msn.com>
2020-08-27 01:42:56 -0300

    Scaffold de Identity con Registro basico de usuarios

---
+++ UI/Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,29 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Web.Areas.Identity.Data;
+using Web.Data;
+
+[assembly: HostingStartup(typeof(Web.Areas.Identity.IdentityHostingStartup))]
+namespace Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+
+                services.AddDbContext<WebIdentityDbContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("DefaultConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<WebIdentityDbContext>();
+            });
+        }
+    }
+}
